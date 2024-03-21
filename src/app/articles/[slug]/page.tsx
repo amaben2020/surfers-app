34336937e064251c9f0e3410abe045102e75ab77
algomachine007 <amaben202phantom@gmail.com>
2024-03-21 20:29:35 +0100

    generate static params for faster build

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,16 +1,25 @@
-import { getArticle } from "@/lib/api";
+import { getAllArticles, getArticle } from "@/lib/api";
 
-const BlogPost = async ({ params }: { params: { slug: string } }) => {
+export async function generateStaticParams() {
+  // grabbing all the article slugs and cache during build time
+  const allArticles = await getAllArticles();
+
+  return allArticles.map((article: any) => ({
+    slug: article.slug,
+  }));
+}
+
+const KnowledgeArticle = async ({ params }: { params: { slug: string } }) => {
   console.log(params);
   const article = await getArticle(params.slug);
 
   console.log(article);
   return (
     <div>
-      BlogPost
+      KnowledgeArticle
       <h1>{article.title}</h1>
     </div>
   );
 };
 
-export default BlogPost;
+export default KnowledgeArticle;
