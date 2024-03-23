de06a97c95f914005e011df8d1a6a27fafa56cfb
algomachine007 <amaben202phantom@gmail.com>
2024-03-23 11:17:21 +0100

    building reusable data fetch in contentful

--- src/lib/blogApi.ts
+++ src/lib/blogApi.ts
@@ -1,12 +1,27 @@
 // GraphQL Content API
 
 const BASE_URL = "https://graphql.contentful.com";
-const configureContentfulUrl = (space: string, environments: string) => {
-  if (
-    (!space && !environments) ||
-    (typeof space !== "string" && typeof space !== "string")
-  ) {
+const configureContentfulUrl = (space: string) => {
+  if (!space || typeof space !== "string") {
     throw Error("Space and Environments must be provided");
   }
-  return `${BASE_URL}/content/v1/spaces/${space}/environments/${environments}`;
+  return `${BASE_URL}/content/v1/spaces/${space}`;
+};
+
+const fetchContentfulData = async (query: string, preview = false) => {
+  try {
+    const url = configureContentfulUrl(process.env.CONTENTFUL_SPACE_ID!);
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
