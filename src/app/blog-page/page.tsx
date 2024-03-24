a30c04000db92eeba98bffc3ddf533ab2aed3b4b
algomachine007 <amaben202phantom@gmail.com>
2024-03-24 07:13:48 +0100

    building hero

--- src/app/blog-page/page.tsx
+++ src/app/blog-page/page.tsx
@@ -10,14 +10,23 @@ const BlogPage = async () => {
     landingPage.topPostsCollection.items,
   );
   return (
-    <div>
-      BlogPage
+    <div className="flex flex-col gap-y-10">
       <p className="text-black"> {JSON.stringify(landingPage)}</p>
       <h2 className="text-black text-2xl">{landingPage.title}</h2>
-      <div className="flex gap-3">
+
+      <div className="mb-10 block">
+        {/* <Image
+          className="max-h-[200px]"
+          src={landingPage.hero.image.url}
+          alt="Image"
+          fill
+        /> */}
+      </div>
+
+      <div className="flex gap-6 mt-4">
         {landingPage.topPostsCollection.items?.map((item: any) => (
           <div key={item.title} className="block">
-            <p className="text-black">{item.title}</p>
+            <p className="text-black font-bold text-2xl">{item.title}</p>
             <Image src={item.image?.url} alt="Image" width={400} height={400} />
           </div>
         ))}
