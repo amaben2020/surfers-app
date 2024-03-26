be578ad24581901e885f87f880ba3fbadc17ac77
algomachine007 <amaben202phantom@gmail.com>
2024-03-26 19:32:00 +0100

    optional chaining for articles

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -7,8 +7,8 @@ export async function generateStaticParams() {
   // grabbing all the article slugs and cache during build time
   const allArticles = await getAllArticles();
 
-  return allArticles.map((article: any) => ({
-    slug: article.slug,
+  return allArticles?.map((article: any) => ({
+    slug: article?.slug,
   }));
 }
 
@@ -21,10 +21,10 @@ const KnowledgeArticle = async ({ params }: { params: { slug: string } }) => {
         <div className="container space-y-12 px-4 md:px-6">
           <div className="space-y-4">
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl text-black">
-              {article.title}
+              {article?.title}
             </h1>
             <p className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
-              {article.summary}
+              {article?.summary}
             </p>
           </div>
           <div className="space-y-8 lg:space-y-10">
@@ -32,13 +32,13 @@ const KnowledgeArticle = async ({ params }: { params: { slug: string } }) => {
               alt="Article Image"
               className="aspect-video w-full overflow-hidden rounded-xl object-cover"
               height="365"
-              src={article.articleImage.url}
+              src={article?.articleImage.url}
               width="650"
             />
             <div className="space-y-4 md:space-y-6">
               <div className="space-y-2">
                 <div className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
-                  {documentToReactComponents(article.details.json)}
+                  {documentToReactComponents(article?.details.json)}
                 </div>
               </div>
             </div>
