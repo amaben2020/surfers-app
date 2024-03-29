afa04f28e720b139314a27421fcde16d4568f550
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 18:21:34 +0100

    hardcoding env

--- src/utils/configManager.ts
+++ src/utils/configManager.ts
@@ -2,7 +2,8 @@ export const configureEnvironment = (environment: string) => {
   switch (environment) {
     case "staging":
       return {
-        accessToken: process.env.CONTENTFUL_STAGING_ACCESS_TOKEN!,
+        accessToken: "woe5rO8bj3-doSNi4ZdgIyU88XtkgjCF549wHeRIAuA",
+        // accessToken: process.env.CONTENTFUL_STAGING_ACCESS_TOKEN!,
         previewToken: process.env.CONTENTFUL_STAGING_PREVIEW_ACCESS_TOKEN!,
       };
 
