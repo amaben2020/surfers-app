876ef0c89c23790d2d7a1493dda48c827b67ed64
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 12:39:23 +0100

    fixing types and build

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,5 +1,5 @@
 //@ts-nocheck
-import { getArticle } from "@/lib/api";
+import { getAllArticles, getArticle } from "@/lib/api";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { draftMode } from "next/headers";
 import Image from "next/image";
--- src/app/blog-page/[slug]/page.tsx
+++ src/app/blog-page/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import RichText from "@/components/RichText";
-import { formatBlogArticlesSlug } from "@/lib/adapters/formatBlogArticlesSlug";
-import { getBlogArticle, getBlogPage } from "@/lib/blogApi";
+import { getBlogArticle } from "@/lib/blogApi";
 import { draftMode } from "next/headers";
 import Image from "next/image";
 
@@ -8,33 +7,33 @@ interface BlogPostPageParams {
   slug: string;
 }
 
-export async function generateStaticParams() {
-  const posts = await getBlogPage();
-  console.log(posts);
-  return formatBlogArticlesSlug(posts);
-}
+// export async function generateStaticParams() {
+//   const posts = await getBlogPage();
+//   console.log(posts);
+//   return formatBlogArticlesSlug(posts);
+// }
 
 // For each blog post, tell Next.js which metadata
 // (e.g. page title) to display.
-export async function generateMetadata({
-  params,
-}: {
-  params: BlogPostPageParams;
-}): Promise<{
-  title: string;
-}> {
-  const data = await getBlogArticle(params.slug, false);
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: BlogPostPageParams;
+// }): Promise<{
+//   title: string;
+// }> {
+//   const data = await getBlogArticle(params.slug, false);
 
-  const blogPost = data?.data?.blogArticleCollection?.items[0];
-  console.log(blogPost.title);
-  // if (!blogPost) {
-  //   return NotFound();
-  // }
+//   const blogPost = data?.data?.blogArticleCollection?.items[0];
+//   console.log(blogPost.title);
+//   // if (!blogPost) {
+//   //   return NotFound();
+//   // }
 
-  return {
-    title: blogPost.title || "",
-  };
-}
+//   return {
+//     title: blogPost.title || "",
+//   };
+// }
 
 const BlogPage = async ({ params }: { params: { slug: string } }) => {
   const { isEnabled } = draftMode();
