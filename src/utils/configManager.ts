759022ea989bd948ad044ac391eef6ad94185954
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 19:19:56 +0100

    fixing configure environment

--- src/utils/configManager.ts
+++ src/utils/configManager.ts
@@ -6,7 +6,7 @@ export const configureEnvironment = (environment: string) => {
         previewToken: process.env.CONTENTFUL_STAGING_PREVIEW_ACCESS_TOKEN!,
       };
 
-    case "production":
+    case "master":
       return {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
         previewToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN!,
