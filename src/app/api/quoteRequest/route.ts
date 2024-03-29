29a7f96690c536aa016519e7416941e7093ff52d
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 11:58:19 +0100

    installing graphql request

--- src/app/api/quoteRequest/route.ts
+++ src/app/api/quoteRequest/route.ts
@@ -1,13 +1,14 @@
 import contentfulClient from "@/utils/contentfulClient";
 import { NextRequest, NextResponse } from "next/server";
 
+// using Contentful as a backend
 export const POST = async (request: NextRequest) => {
   const { name, address, email, price } = await request.json();
 
   const newEntry = contentfulClient.entry.create(
     {
       spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environmentId: "staging",
+      environmentId: process.env.CONTENTFUL_ENVIRONMENT,
       contentTypeId: "quoteRequest",
     },
     {
