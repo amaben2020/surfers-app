b433a965096af4a3a43a4f52a41f4984aba21417
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 18:27:19 +0100

    undoing hardcoding of env

--- src/utils/configManager.ts
+++ src/utils/configManager.ts
@@ -2,8 +2,7 @@ export const configureEnvironment = (environment: string) => {
   switch (environment) {
     case "staging":
       return {
-        accessToken: "woe5rO8bj3-doSNi4ZdgIyU88XtkgjCF549wHeRIAuA",
-        // accessToken: process.env.CONTENTFUL_STAGING_ACCESS_TOKEN!,
+        accessToken: process.env.CONTENTFUL_STAGING_ACCESS_TOKEN!,
         previewToken: process.env.CONTENTFUL_STAGING_PREVIEW_ACCESS_TOKEN!,
       };
 
