24e3e75e1a5d45581a35abe32f42938cd213b778
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 09:45:42 +0100

    fetching data based on environment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,11 @@ import Link from "next/link";
 
 export default async function Home() {
   const { isEnabled } = draftMode();
- 
-  const articles = await getAllArticles(3, isEnabled);
+
+  const articles = await getAllArticles(4, isEnabled);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-   
       <section className="w-full pt-12">
         <div className="mx-auto container space-y-12 px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -36,28 +35,28 @@ export default async function Home() {
                     alt="placeholder"
                     className="aspect-[4/3] object-cover w-full"
                     height="263"
-                    src={article.articleImage.url}
+                    src={article?.articleImage?.url}
                     width="350"
                   />
                   <div className="flex-1 p-6">
                     <Link href={`/articles/${article.slug}`}>
                       <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
-                        {article.title}
+                        {article?.title}
                       </h3>
                     </Link>
                     <div className="inline-block rounded-full bg-zinc-100 px-3 py-1 text-sm font-semibold text-zinc-800">
-                      {article.categoryName}
+                      {article?.categoryName}
                     </div>
                     <p className="max-w-none text-zinc-500 mt-4 mb-2 text-sm dark:text-zinc-400">
-                      {article.summary}
+                      {article?.summary}
                     </p>
                     <p className="max-w-none text-zinc-600 mt-2 mb-2 text-sm font-bold dark:text-zinc-400">
-                      Written by: {article.authorName}
+                      Written by: {article?.authorName}
                     </p>
                     <div className="flex justify-end">
                       <Link
                         className="inline-flex h-10 items-center justify-center text-sm font-medium"
-                        href={`/articles/${article.slug}`}
+                        href={`/articles/${article?.slug}`}
                       >
                         Read More →
                       </Link>
