07e349d5bced52db99bf021f5bd6a804cf2fa0a9
algomachine007 <amaben202phantom@gmail.com>
2024-03-29 12:17:58 +0100

    articles build fix

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -6,15 +6,18 @@ import Image from "next/image";
 export async function generateStaticParams() {
   // grabbing all the article slugs and cache during build time
   const allArticles = await getAllArticles();
-
-  return allArticles?.map((article: any) => ({
-    slug: article?.slug,
-  }));
+  console.log(allArticles);
+  return (
+    Array.isArray(allArticles) &&
+    allArticles?.map((article: any) => ({
+      slug: article?.slug,
+    }))
+  );
 }
 
 const KnowledgeArticle = async ({ params }: { params: { slug: string } }) => {
   const { isEnabled } = draftMode();
-  const article = await getArticle(params.slug, isEnabled);
+  const article = await getArticle(params?.slug, isEnabled);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <section className="w-full">
@@ -32,13 +35,13 @@ const KnowledgeArticle = async ({ params }: { params: { slug: string } }) => {
               alt="Article Image"
               className="aspect-video w-full overflow-hidden rounded-xl object-cover"
               height="365"
-              src={article?.articleImage.url}
+              src={article?.articleImage?.url}
               width="650"
             />
             <div className="space-y-4 md:space-y-6">
               <div className="space-y-2">
                 <div className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
-                  {documentToReactComponents(article?.details.json)}
+                  {documentToReactComponents(article?.details?.json)}
                 </div>
               </div>
             </div>
